c026ed6c06a842ab2bcf7afdc813e3b059d1bfb9
thejaswani chatta <thejaswani.chatta@gmail.com>
2025-08-28 16:06:49 +0200

    added UI dist files to NestJs project

--- portfolio-be/src/main.ts
+++ portfolio-be/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Serve static Angular files
+  app.use(express.static(join(__dirname, '..', 'public')));
+
+  // Redirect all routes to Angular index.html
+  app.get('*', (_req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
    app.enableCors({
     origin: 'http://localhost:4200', // your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
